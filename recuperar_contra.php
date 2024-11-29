29d4f09cfe4af497d190fb45bc57545c0fa2ae80
Rodrigo Olivares <rodrigo.olivares975@gmail.com>
2024-11-29 02:10:42 -0300

    No funciono mailhog pero hce algo mas simple

--- recuperar_contra.php
+++ recuperar_contra.php
@@ -5,13 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recuperar Contraseña</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Recuperar Contraseña</h1>
-    <form action="recuperar.php" method="POST">
-        <label for="email">Correo Electrónico:</label>
-        <input type="email" id="email" name="email" required>
-        <button type="submit">Recuperar Contraseña</button>
-    </form>
+    <div class="container mt-5">
+        <h2 class="text-center">Recuperar Contraseña</h2>
+            <form action="validar_correo.php" method="POST" class="mt-4">
+                <div class="mb-3">
+                    <label for="email" class="form-label">Ingresa tu correo</label>
+                    <input type="email" name="email" class="form-control" id="email" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </form>
+    </div>        
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
 </body>
 </html>
